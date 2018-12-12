27727e3102a8a9997a369220aa8803eae99b4c05
wyanassert <1402099772@qq.com>
2018-12-12 20:36:33 +0800

    0.1.6 fix bug in UIImage+Mask

--- WYUtils/Classes/UIImage/UIImage+WYMask.m
+++ WYUtils/Classes/UIImage/UIImage+WYMask.m
@@ -17,9 +17,23 @@
     CGFloat scale = MIN(size0.width / size1.width, size0.height/size1.height);
     
     CGRect rect = CGRectMake((size0.width - scale * size1.width)/2, (size0.height - scale * size1.height)/2, scale * size1.width, scale * size1.height);
+    CGRect rect0;
+    CGRect rect1;
+    if(size0.height/size1.height < size0.width / size1.width) {
+        rect0 = CGRectMake(0, 0, (size0.width - rect.size.width)/2, size0.height);
+        rect1 = CGRectMake((size0.width + rect.size.width)/2, 0, (size0.width - rect.size.width)/2, size0.height);
+    } else {
+        rect0 = CGRectMake(0, 0, size0.width, (size0.height - rect.size.height)/2);
+        rect1 = CGRectMake(0, (size0.height + rect.size.height)/2, size0.width, (size0.height - rect.size.height)/2);
+    }
     
     UIGraphicsBeginImageContextWithOptions(size0, NO, 0.0);
     [[UIColor clearColor] set];
+    
+    [[UIColor whiteColor] setFill];
+    UIRectFill(rect0);
+    UIRectFill(rect1);
+    
     [maskImage drawInRect:rect];
     maskImage = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
